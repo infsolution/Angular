204f4b3af9c9662382a58a5502684e94c4e5cd28
infsolution <clsinfsolution@gmail.com>
2019-07-13 10:40:51 -0300

    add corsheaders

--- petshop/petshop/settings.py
+++ petshop/petshop/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
